056e606615f4beb211e3a3c820343ff4fbce3206
Gabriel Malkas <gabriel.malkas@gmail.com>
2020-04-01 10:41:58 -0700

    Fix if tests in setup.sh and teardown.sh

--- setup.sh
+++ setup.sh
@@ -3,16 +3,16 @@
 set -e
 
 SE_CONTAINER_NAME=beaverlabs_build
-SE_CONTAINER_EXISTS=$(lxc-ls | grep "^$SE_CONTAINER_NAME")
-SE_CONTAINER_RUNNING=$(lxc-ls --running | grep "^$SE_CONTAINER_NAME")
+SE_CONTAINER_EXISTS=$(lxc-ls | grep "^$SE_CONTAINER_NAME" | sed 's/ *$//g')
+SE_CONTAINER_RUNNING=$(lxc-ls --running | grep "^$SE_CONTAINER_NAME" | sed 's/ *$//g')
 
-if [[ "$SE_CONTAINER_EXISTS" == "$SE_CONTAINER_NAME" ]]; then
+if [[ "$SE_CONTAINER_EXISTS" != "$SE_CONTAINER_NAME" ]]; then
   echo "Creating container..."
   lxc-create -t download -n $SE_CONTAINER_NAME -- -d debian -r stretch -a amd64
 fi
 
 
-if [[ "$SE_CONTAINER_RUNNING" == "$SE_CONTAINER_NAME" ]]; then
+if [[ "$SE_CONTAINER_RUNNING" != "$SE_CONTAINER_NAME" ]]; then
   echo "Starting container..."
   lxc-start -n $SE_CONTAINER_NAME
 fi
